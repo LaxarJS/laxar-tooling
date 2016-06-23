41085898c65171a9df5256337b79509d34b93d79
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-06-23 15:36:16 +0200

    (wip, #1) use strict

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 exports.artifactCollector = require( './lib/artifact_collector' );
 exports.helpers = require( './lib/helpers' );
 exports.collectResources = require( './lib/resources' );
