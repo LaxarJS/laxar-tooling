a314cc036028d4f70fee54e3a2a2ffd50e0fa523
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-06-23 16:19:14 +0200

    (wip, #1) move getResourcePaths to resources.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 
 exports.artifactCollector = require( './lib/artifact_collector' );
 exports.helpers = require( './lib/helpers' );
-exports.collectResources = require( './lib/resources' );
+exports.collectResources = require( './lib/resources' ).collectResources;
+exports.getResourcePaths = require( './lib/resources' ).getResourcePaths;
