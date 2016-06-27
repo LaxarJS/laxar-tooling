61fc9ac150e6859c800a9faa4d1c8547659b2e9b
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-06-27 12:58:12 +0200

    (#1) refactor, extract file_reader for future webpack loader injection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 exports.artifactCollector = require( './lib/artifact_collector' );
-exports.helpers = require( './lib/helpers' );
+exports.jsonReader = require( './lib/json_reader' );
+exports.fileReader = require( './lib/file_reader' );
 exports.collectResources = require( './lib/resources' ).collectResources;
 exports.getResourcePaths = require( './lib/resources' ).getResourcePaths;
