1e99f9f8a9e21c5e01bb87324ba4d770b2b0fc09
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-06-27 14:14:15 +0200

    (#1) refactor resource collector (wip)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.artifactCollector = require( './lib/artifact_collector' );
+exports.resourceCollector = require( './lib/resource_collector' );
 exports.jsonReader = require( './lib/json_reader' );
 exports.fileReader = require( './lib/file_reader' );
-exports.collectResources = require( './lib/resources' ).collectResources;
-exports.getResourcePaths = require( './lib/resources' ).getResourcePaths;
+exports.getResourcePaths = exports.resourceCollector.getResourcePaths;
