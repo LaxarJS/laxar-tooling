5f7e7cbd0263a29dec9235b3d88dbc2e0bcb77ba
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-06-28 10:54:36 +0200

    (#1) async projectPath(), added dependency_collector

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 exports.artifactCollector = require( './lib/artifact_collector' );
 exports.resourceCollector = require( './lib/resource_collector' );
+exports.dependencyCollector = require( './lib/dependency_collector' );
 exports.jsonReader = require( './lib/json_reader' );
 exports.fileReader = require( './lib/file_reader' );
 exports.getResourcePaths = exports.resourceCollector.getResourcePaths;
