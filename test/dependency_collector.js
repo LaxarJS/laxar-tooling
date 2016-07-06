539440890d7ba53eeb7f6c2bd24fbea7c5a09d92
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-07-06 14:28:59 +0200

    Add tests for dependency collector

--- test/dependency_collector.js
+++ test/dependency_collector.js
@@ -42,5 +42,28 @@ describe( 'dependencyCollector', function() {
 
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+   describe( '.collectResource( artifacts )', function() {
+
+      var collector = dependencyCollector.create( log, {} );
+
+      var data = require( './data/dependencies.json' );
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      it( 'returns a thenable', function() {
+         expect( collector.collectDependencies( data.artifacts.empty ) ).to.respondTo( 'then' );
+      } );
+
+      it( 'groups dependencies by integration technology', function() {
+         return collector.collectDependencies( data.artifacts.minimal )
+            .then( function( dependencies ) {
+               expect( dependencies ).to.contain.a.key( 'plain' );
+               expect( dependencies.plain ).to.have.a.lengthOf( 2 );
+            } );
+      } );
+
+   } );
+
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 } );
