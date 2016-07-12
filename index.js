a86e540a8bd6d92ad3657f8937b8b0169bf53407
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-07-12 14:53:29 +0200

    Refactor, add stylesheet collector

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 exports.artifactCollector = require( './lib/artifact_collector' );
 exports.resourceCollector = require( './lib/resource_collector' );
 exports.dependencyCollector = require( './lib/dependency_collector' );
+exports.stylesheetCollector = require( './lib/stylesheet_collector' );
 exports.jsonReader = require( './lib/json_reader' );
 exports.fileReader = require( './lib/file_reader' );
 exports.getResourcePaths = exports.resourceCollector.getResourcePaths;
